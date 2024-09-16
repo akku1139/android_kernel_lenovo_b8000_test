ad5a36136230e12bec11f8366e6c25c0a2e23f05
akku <akku@akku.dedyn.io>
2024-09-17 00:55:53 +0900

    Update toolchain path

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ makedefs="V=0"
 #makedefs="V=1"
 makejobs=${MAKEJOBS}
 curdir=`pwd`
-TOOLCHAIN="./toolchain/arm-cortex_a7-linux-gnueabihf_4.9.3/bin"
+TOOLCHAIN="./toolchain/arm-cortex_a7-linux-gnueabihf-linaro_4.9/bin"
 #if [ "${KBUILD_OUTPUT_SUPPORT}" == "yes" ];then
 #  outdir=$curdir/out
 #  mkdir -p $outdir
